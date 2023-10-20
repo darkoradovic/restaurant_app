01406ba2d5545e81a68257fd2edd2b276bbbc441
darkoradovic <darmior5@gmail.com>
2023-10-20 08:21:55 +0200

    Feat: add products

--- src/app/api/categories/route.ts
+++ src/app/api/categories/route.ts
@@ -1,16 +1,17 @@
-import { prisma } from "@/libs/prismadb"
-import { NextResponse } from "next/server"
+import { prisma } from "@/libs/prismadb";
+import { NextResponse } from "next/server";
 
+
+// FETCH ALL CATEGORIES
 export const GET = async () => {
-   try {
-    const categories = await prisma.category.findMany()
-    return new NextResponse(JSON.stringify(categories), {status: 200})
-     
-   } catch (error) {
-    return new NextResponse(JSON.stringify({message: "Something went wrong!"}), {status: 500})
-   }
-}
-
-export const POST = () => {
-    return new NextResponse("Hello", {status: 200})
-}
+  try {
+    const categories = await prisma.category.findMany();
+    return new NextResponse(JSON.stringify(categories), { status: 200 });
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
---
+++ src/app/api/products/route.ts
@@ -0,0 +1,22 @@
+import { prisma } from "@/libs/prismadb"
+import { NextRequest, NextResponse } from "next/server"
+
+export const GET = async (req: NextRequest) => {
+    const { searchParams } = new URL(req.url);
+    const cat = searchParams.get("cat");
+  
+    try {
+      const products = await prisma.product.findMany({
+        where: {
+          ...(cat ? { catSlug: cat } : { isFeatured: true }),
+        },
+      });
+      return new NextResponse(JSON.stringify(products), { status: 200 });
+    } catch (err) {
+      console.log(err);
+      return new NextResponse(
+        JSON.stringify({ message: "Something went wrong!" }),
+        { status: 500 }
+      );
+    }
+  };
