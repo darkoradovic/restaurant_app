788d7db8c8b586ab114997d5275d047f3fd98ec4
darkoradovic <darmior5@gmail.com>
2023-10-21 18:21:55 +0200

    Feat: add stripe payment method

--- src/app/api/orders/[id]/route.ts
+++ src/app/api/orders/[id]/route.ts
@@ -1,24 +1,32 @@
 import { prisma } from "@/libs/prismadb";
 import { NextRequest, NextResponse } from "next/server";
 
-export const PUT = async (req: NextRequest, {params}:{params:{id: string}}) => {
-    const {id} = params
-    const body = await req.json()
-    try {
-        await prisma.order.update({
-            where:{
-                id: id
-            },
-            data: {status: body}
-        })
-        return new NextResponse(
-            JSON.stringify({ message: "Order updated" }),
-            { status: 200 }
-          );
-    } catch (error) {
-        return new NextResponse(
-            JSON.stringify({ message: "Failed to update" }),
-            { status: 500 }
-          );
-    }
-}
+
+// CHANGE THE STATUS OF AN ORDER
+export const PUT = async (
+  req: NextRequest,
+  { params }: { params: { id: string } }
+) => {
+  const { id } = params;
+
+  try {
+    const body = await req.json();
+
+    await prisma.order.update({
+      where: {
+        id: id,
+      },
+      data: { status: body },
+    });
+    return new NextResponse(
+      JSON.stringify({ message: "Order has been updated!" }),
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }),
+      { status: 500 }
+    );
+  }
+};
